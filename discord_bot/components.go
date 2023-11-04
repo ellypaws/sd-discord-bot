6f8b752f0f3b5b136e3f9f73418965a08c21eda1
Elly <dev@keiau.space>
2023-11-04 10:12:17 +0800

    Move some component handlers

--- discord_bot/components.go
+++ discord_bot/components.go
@@ -3,6 +3,7 @@ package discord_bot
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"log"
 	"stable_diffusion_bot/entities"
 )
 
@@ -119,5 +120,102 @@ func (b *botImpl) settingsMessageComponents(settings *entities.DefaultSettings) 
 				},
 			},
 		},
+	}
+}
+
+func (b *botImpl) processImagineDimensionSetting(s *discordgo.Session, i *discordgo.InteractionCreate, height, width int) {
+	botSettings, err := b.imagineQueue.UpdateDefaultDimensions(width, height)
+	if err != nil {
+		log.Printf("error updating default dimensions: %v", err)
+
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseUpdateMessage,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Error updating default dimensions...",
+			},
+		})
+		if err != nil {
+			log.Printf("Error responding to interaction: %v", err)
+		}
+
+		return
+	}
+
+	messageComponents := b.settingsMessageComponents(botSettings)
+
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseUpdateMessage,
+		Data: &discordgo.InteractionResponseData{
+			Content:    "Choose defaults settings for the imagine command:",
+			Components: messageComponents,
+		},
+	})
+	if err != nil {
+		log.Printf("Error responding to interaction: %v", err)
+		return
+	}
+}
+
+func (b *botImpl) processImagineBatchSetting(s *discordgo.Session, i *discordgo.InteractionCreate, batchCount, batchSize int) {
+	botSettings, err := b.imagineQueue.UpdateDefaultBatch(batchCount, batchSize)
+	if err != nil {
+		log.Printf("error updating batch settings: %v", err)
+
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseUpdateMessage,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Error updating batch settings...",
+			},
+		})
+		if err != nil {
+			log.Printf("Error responding to interaction: %v", err)
+		}
+
+		return
+	}
+
+	messageComponents := b.settingsMessageComponents(botSettings)
+
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseUpdateMessage,
+		Data: &discordgo.InteractionResponseData{
+			Content:    "Choose defaults settings for the imagine command:",
+			Components: messageComponents,
+		},
+	})
+	if err != nil {
+		log.Printf("Error responding to interaction: %v", err)
+	}
+}
+
+func (b *botImpl) processImagineSDModelNameSetting(s *discordgo.Session, i *discordgo.InteractionCreate, newModelName string) {
+	botSettings, err := b.imagineQueue.UpdateModelName(newModelName)
+	if err != nil {
+		log.Printf("error updating sd model name settings: %v", err)
+
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseUpdateMessage,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Error updating sd model name settings...",
+			},
+		})
+		if err != nil {
+			log.Printf("Error responding to interaction: %v", err)
+		}
+
+		return
+	}
+
+	messageComponents := b.settingsMessageComponents(botSettings)
+
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseUpdateMessage,
+		Data: &discordgo.InteractionResponseData{
+			Content:    "Choose defaults settings for the imagine command:",
+			Components: messageComponents,
+		},
+	})
+	if err != nil {
+		log.Printf("Error responding to interaction: %v", err)
 	}
 }
