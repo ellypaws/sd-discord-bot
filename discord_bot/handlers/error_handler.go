8ca1dc812ee6d3abb16d248640b5bafe229aa96f
Elly <145195760+ellypaws@users.noreply.github.com>
2024-01-22 16:51:40 +0800

    Change deprecated ErrorHandler to be a function alias

--- discord_bot/handlers/error_handler.go
+++ discord_bot/handlers/error_handler.go
@@ -30,35 +30,7 @@ var Errors = map[errorEnum]errorResponseType{
 
 // ErrorHandler responds to the interaction with an error message and a deletion button.
 // Deprecated: Use ErrorEdit instead.
-func ErrorHandler(s *discordgo.Session, i *discordgo.Interaction, errorContent ...any) {
-	Errors[ErrorResponse](s, i, errorContent...)
-	//var errorString string
-	//
-	//switch content := errorContent.(type) {
-	//case string:
-	//	errorString = content
-	//case error:
-	//	errorString = fmt.Sprint(content) // Convert the error to a string
-	//default:
-	//	errorString = "An unknown error has occurred"
-	//}
-	//components := []discordgo.MessageComponent{
-	//	discordgo.ActionsRow{
-	//		Components: []discordgo.MessageComponent{
-	//			discordgo.Button{
-	//				Label:    "Delete this message",
-	//				Style:    discordgo.DangerButton,
-	//				CustomID: "delete_error_message",
-	//			},
-	//		},
-	//	},
-	//}
-	//
-	//_, _ = s.InteractionResponseEdit(i, &discordgo.WebhookEdit{
-	//	Content:    &errorString,
-	//	Components: &components,
-	//})
-}
+var ErrorHandler = Errors[ErrorResponse]
 
 func CheckAPIAlive(apiHost string) bool {
 	resp, err := http.Get(apiHost)
