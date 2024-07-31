f3724caff6589a0497e09b385a0a30c882e39a8b
Elly <145195760+ellypaws@users.noreply.github.com>
2024-07-31 15:48:49 +0800

    perf: use sync.Pool on AsyncImage fix: implement MarshalJSON for (*Image)

--- utils/image.go
+++ utils/image.go
@@ -9,10 +9,16 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"sync"
 )
 
+var asyncPool = sync.Pool{New: newAsync}
+
+// AsyncImage returns an *Image that asynchronously downloads the image from the given URL as the object is created.
+// The returned data is in base64 format.
 func AsyncImage(url string) *Image {
-	result := Image{ch: make(chan []byte)}
+	result := asyncPool.Get().(*Image)
+	result.Reset()
 
 	go func() {
 		defer close(result.ch)
@@ -25,26 +31,91 @@ func AsyncImage(url string) *Image {
 		result.ch <- []byte(data)
 	}()
 
-	return &result
+	return result
 }
 
 type Image struct {
 	ch     chan []byte
 	err    error
 	buffer bytes.Buffer
+	id     int
+	closed bool
 }
 
-func (r *Image) Read(b []byte) (int, error) {
+// flush writes the data from the channel to the buffer, waiting until the data is ready.
+// multiple calls to flush will simultaneously unlock once the channel is closed
+func (r *Image) flush() {
 	bin, ok := <-r.ch
 	if ok {
 		r.buffer.Write(bin)
 	}
+}
+
+func (r *Image) Read(b []byte) (int, error) {
+	r.flush()
 
 	if r.err != nil {
 		return 0, r.err
 	}
 
-	return r.buffer.Read(b)
+	var i int
+	i, r.err = r.buffer.Read(b)
+	if r.err == io.EOF {
+		defer r.close()
+	}
+
+	return i, r.err
+}
+
+func (r *Image) Bytes() []byte {
+	r.flush()
+	return r.buffer.Bytes()
+}
+
+func (r *Image) String() string {
+	r.flush()
+	return r.buffer.String()
+}
+
+func (r *Image) Buffer() *bytes.Buffer {
+	r.flush()
+	return &r.buffer
+}
+
+var asyncID int
+
+func newAsync() any {
+	async := Image{id: asyncID}
+	asyncID++
+	return &async
+}
+
+func (r *Image) Reset() {
+	r.ch = make(chan []byte)
+	r.closed = false
+	r.err = nil
+	r.buffer.Reset()
+}
+
+func (r *Image) close() {
+	if r.closed {
+		return
+	}
+	r.err = io.EOF
+	r.buffer.Reset()
+	asyncPool.Put(r)
+}
+
+func (r *Image) MarshalJSON() ([]byte, error) {
+	r.flush()
+	out := make([]byte, r.Buffer().Len()+2)
+	out[0] = '"'
+	read, err := r.Read(out[1:])
+	if err != nil {
+		return nil, err
+	}
+	out[read+1] = '"'
+	return out, nil
 }
 
 func GetDataFromUrl(url string) ([]byte, error) {
